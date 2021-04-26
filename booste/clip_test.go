06f8814ed6977becd5d143b8a6977119f5a440fd
erik-dunteman <erikdunteman@gmail.com>
2021-04-26 13:06:21 -0500

    Added clip method

---
+++ booste/clip_test.go
@@ -0,0 +1,18 @@
+package booste
+
+import (
+	"fmt"
+	"os"
+	"testing"
+)
+
+func TestClip(t *testing.T) {
+	apiKey := os.Getenv("API_KEY")
+	prompts := []string{"A basketball", "A football"}
+	images := []string{"https://upload.wikimedia.org/wikipedia/commons/7/7a/Basketball.png"}
+	sims, err := CLIP(apiKey, prompts, images)
+	if err != nil {
+		t.Fatal(err)
+	}
+	fmt.Printf("%+v\n", sims)
+}
