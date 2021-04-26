c056ccb6ab5eeb1617a371b7f7f60cb9fabe52f3
erik-dunteman <erikdunteman@gmail.com>
2021-04-26 13:14:31 -0500

    Comment change

--- booste/clip.go
+++ booste/clip.go
@@ -2,8 +2,8 @@ package booste
 
 // THIS FILE WILL BE DEPRECIATED SOON
 
-// With other language clients, we've ran GPT2 through booste.gpt2,
-// and we're migrating to booste.run("gpt2") syntax to keep the clientside API
+// With other language clients, we've ran CLIP through booste.clip,
+// and we're migrating to booste.run("clip") syntax to keep the clientside API
 // simple and unchanging as more models are added.
 
 import (
